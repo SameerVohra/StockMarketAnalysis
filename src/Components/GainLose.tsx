f18cd31cf543b7f1bec0e82b90627b62e9322006
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-07-22 19:41:02 +0530

    fixed error

--- src/Components/GainLose.tsx
+++ src/Components/GainLose.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Table from "./Table";
 
 const GainLost = () => {
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import GainLost from "./GainLose";
 
 function Home(){
